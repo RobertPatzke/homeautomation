47fafdc7c0c58807c624061743ada9e4ea994809
Robert Patzke <robert.patzke@mfp-portal.de>
2017-10-27 18:33:14 +0200

    Correction of Broadcast Address Name

--- libraries/environment/socManNetUser.h
+++ libraries/environment/socManNetUser.h
@@ -71,7 +71,7 @@
 #define BROADCAST_IP_B2 99
 #define BROADCAST_IP_B3 255
 
-#define LOCAL_IP_ADR_STR (char *) "192.168.99.255"
+#define BROADCAST_IP_ADR_STR (char *) "192.168.99.255"
 
 // IP-Gateway
 // ----------------------------------------------

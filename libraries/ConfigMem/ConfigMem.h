0cc26ed363cad4286fa437e20e1889b67592cc9f
Prof. Dr.-Ing. Robert Patzke <32030282+RobertPatzke@users.noreply.github.com>
2018-01-20 08:36:37 +0100

    Create ConfigMem.h

---
+++ libraries/ConfigMem/ConfigMem.h
@@ -0,0 +1,10 @@
+//-----------------------------------------------------------------------------
+// Thema:   Configuration Memory / Upload/Download and Management
+// Datei:   ConfigMem.h
+// Editor:  Robert Patzke
+// URI/URL: www.mfp-portal.de
+//-----------------------------------------------------------------------------
+// Lizenz:  CC-BY-SA  (siehe Wikipedia: Creative Commons)
+//
+
+#include "environment.h"

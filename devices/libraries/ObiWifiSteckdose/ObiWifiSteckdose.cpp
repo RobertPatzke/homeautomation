09586b7ba8c04fe0aa566ec561879c1f7a666ba5
Robert Patzke <robert.patzke@mfp-portal.de>
2018-04-14 20:55:28 +0200

    Start library for OBI WiFi Steckdose

---
+++ devices/libraries/ObiWifiSteckdose/ObiWifiSteckdose.cpp
@@ -0,0 +1,21 @@
+// ---------------------------------------------------------------------------
+// File:        ObiWifiSteckdose.h
+// Editors:     Robert Patzke,
+// Start:       14. April 2018
+// Last change: 14. April 2018
+// URI/URL:     www.mfp-portal.de, homeautomation.x-api.de
+// Licence:     Creative Commons CC-BY-SA
+// ---------------------------------------------------------------------------
+//
+
+#include "ObiWifiSteckdose.h"
+
+// ---------------------------------------------------------------------------
+// Konstruktoren und Initialisierungen
+// ---------------------------------------------------------------------------
+//
+
+ObiWifiSteckdose::ObiWifiSteckdose(int cycleTime)
+{
+
+}

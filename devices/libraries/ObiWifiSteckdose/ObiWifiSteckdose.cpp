d005643a5d96c17766fa5112120d00237e2fa631
Robert Patzke <robert.patzke@mfp-portal.de>
2018-04-15 20:12:17 +0200

    Creating button behaviour for input pin

--- devices/libraries/ObiWifiSteckdose/ObiWifiSteckdose.cpp
+++ devices/libraries/ObiWifiSteckdose/ObiWifiSteckdose.cpp
@@ -11,11 +11,141 @@
 #include "ObiWifiSteckdose.h"
 
 // ---------------------------------------------------------------------------
-// Konstruktoren und Initialisierungen
+// constructors and initialisations
 // ---------------------------------------------------------------------------
 //
 
-ObiWifiSteckdose::ObiWifiSteckdose(int cycleTime)
+ObiWifiSteckdose::ObiWifiSteckdose(int cycTime)
 {
+  cycleTime = cycTime;
+  frequency = 1000 / cycleTime;
+}
 
+
+// ---------------------------------------------------------------------------
+// user functions
+// ---------------------------------------------------------------------------
+//
+void ObiWifiSteckdose::begin()
+{
+#ifdef useArduinoLibs
+  pinMode(button, INPUT_PULLUP);
+  pinMode(pinLed, OUTPUT);
+  pinMode(relayON, OUTPUT);
+  pinMode(relayOff, OUTPUT);
+
+  digitalWrite(pinLed, HIGH);
+  digitalWrite(relayON, HIGH);
+  digitalWrite(relayOff, HIGH);
+#endif
+
+  buttonCheckState      = bcsIdle;
+  buttonUpIdleTime      = buttonIdleTime / cycleTime;
+  buttonUpTime          = buttonUpIdleTime;
+  buttonDownDetectTime  = buttonDetectTime / cycleTime;
+  buttonUpDetectTime    = buttonDownDetectTime;
+  buttonUpClickTime     = buttonClickTime / cycleTime;
+  buttonDownTime        = 0;
+  buttonClickCounter    = 0;
+  buttonReset           = false;
+}
+
+void ObiWifiSteckdose::run()
+{
+  // check button
+  //
+  if(digitalRead(button) == LOW)
+  {
+    buttonDownTime++;
+    buttonUpTime = 0;
+  }
+  else
+  {
+    buttonUpTime++;
+    buttonDownTime = 0;
+  }
+
+  // evaluate button state
+  //
+  switch(buttonCheckState)
+  {
+    case bcsIdle:
+    // ------------------------------------------------------------------------
+      if(buttonUpTime > buttonUpIdleTime)
+      {
+        // if button is not touched
+        // simply limit up time value
+        //
+        buttonUpTime = buttonUpIdleTime;
+        break;
+      }
+
+      if(buttonDownTime > buttonDownDetectTime)
+      {
+        // if button is pushed
+        // wait until down time is long enough
+        // for marking a click of button
+        //
+        buttonClickCounter++;
+        buttonCheckState = bcsLow;
+      }
+      break;
+
+    case bcsLow:
+    // ------------------------------------------------------------------------
+      if(buttonDownTime > buttonDownResetTime)
+      {
+        // if button is pressed a long time
+        // it indicates a reset
+        //
+        buttonReset = true;
+        buttonDownTime = buttonDownResetTime;
+        break;
+      }
+
+      if(buttonUpTime > buttonUpDetectTime)
+      {
+        // if button is released
+        // wait until up time is long enough
+        // for enter a new state
+        //
+        buttonCheckState = bcsHigh;
+      }
+      break;
+
+    case bcsHigh:
+    // ------------------------------------------------------------------------
+      if(buttonUpTime > buttonUpClickTime)
+      {
+        // button is pressed only for one click
+        //
+        buttonClicked = true;
+        buttonCheckState = bcsIdle;
+        break;
+      }
+
+      if(buttonDownTime > buttonDownDetectTime)
+      {
+        // button is pressed again before click time
+        //
+        buttonMultClicked = true;
+        buttonClickCounter++;
+        buttonCheckState = bcsLow;
+      }
+      break;
+  }
+
+}
+
+int ObiWifiSteckdose::clicks()
+{
+  int retv = 0;
+
+  if(!buttonClicked)
+    return(retv);
+
+  retv = buttonClickCounter;
+  buttonClickCounter = 0;
+  buttonClicked = false;
+  return(retv);
 }
--- devices/libraries/ObiWifiSteckdose/ObiWifiSteckdose.h
+++ devices/libraries/ObiWifiSteckdose/ObiWifiSteckdose.h
@@ -11,11 +11,21 @@
 #ifndef _ObiWifiSteckdose_H
 #define _ObiWifiSteckdose_H
 // ----------------------------------------------------------------------------
+#define useArduinoLibs
 
-#define pinLed      4
-#define relayON     12
-#define relayOff    5
-#define button      14
+#ifdef useArduinoLibs
+  #include "Arduino.h"
+#endif
+
+#define pinLed              4
+#define relayON             12
+#define relayOff            5
+#define button              14
+
+#define buttonIdleTime      2000
+#define buttonResetTime     2000
+#define buttonDetectTime    50
+#define buttonClickTime     1000
 
 // ---------------------------------------------------------------------------
 // class ObiWifiSteckdose
@@ -24,34 +34,67 @@
 class ObiWifiSteckdose
 {
   // -------------------------------------------------------------------------
-  // Klassenspezifische Datentypen
+  // class specific data types
   // -------------------------------------------------------------------------
   //
+  enum ButtonCheckState
+  {
+    bcsIdle,    // button is up (for long time)
+    bcsLow,     // button is down (counting)
+    bcsHigh,    // button is up (counting)
+    bcsNrOfStates
+  };
 
 private:
   // -------------------------------------------------------------------------
-  // Lokale Variablen
+  // local variables
   // -------------------------------------------------------------------------
   //
+  bool relayBlink;
+  bool ledBlink;
+  bool buttonDown;
+  bool buttonReset;
+  bool buttonClicked;
+  bool buttonMultClicked;
 
+  int  buttonDownTime;
+  int  buttonUpTime;
+
+  int  buttonUpIdleTime;
+  int  buttonUpDetectTime;
+  int  buttonUpClickTime;
+  int  buttonDownResetTime;
+  int  buttonDownDetectTime;
+  int  buttonClickCounter;
+
+  ButtonCheckState  buttonCheckState;
 
   // -------------------------------------------------------------------------
-  // Lokale Funktionen
+  // local functions
   // -------------------------------------------------------------------------
   //
 
 public:
   // -------------------------------------------------------------------------
-  // Konstruktoren und Initialisierungen
+  // constructors and initialisations
   // -------------------------------------------------------------------------
   //
   ObiWifiSteckdose(int cycleTime);
 
   // -------------------------------------------------------------------------
-  // Anwenderfunktionen
+  //  global variables
   // -------------------------------------------------------------------------
   //
-  void run();
+  int frequency;
+  int cycleTime;
+
+  // -------------------------------------------------------------------------
+  // user functions
+  // -------------------------------------------------------------------------
+  //
+  void  begin();
+  void  run();
+  int   clicks();
 
 };
 
